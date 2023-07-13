3588cfd499dcd2ea6b9fd0ad56332659b5f64f21
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-13 04:50:12 -0700

    Create file 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: always 1 (failure)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - ");
+	fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+	return (1);
+}

35aedcfcdffced468dd8533a09945f98356b9ca8
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 15:54:05 +0300

    Create program 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

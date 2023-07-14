a19a6b9517c0cbdaa0a1381e8fe99afff0f6bc4f
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 16:02:24 +0300

    Create program 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

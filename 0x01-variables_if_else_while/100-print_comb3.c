42204bc4d3464f4f0678b2423235d4d24c713582
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 17:27:21 +0300

    Create program 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '0' + 10; i++)
+	{
+		for (j = '0'; j < '0' + 10; j++)
+		{
+			if (i == j || j < i)
+				continue;
+
+			putchar(i);
+			putchar(j);
+
+			if (!(i == '8' && j == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

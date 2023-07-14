654b46fc0b8babb88b51fd7351e0c1b730d317c8
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 15:55:34 +0300

    Create program 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

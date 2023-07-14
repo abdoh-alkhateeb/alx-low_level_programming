0d739fbde0ae57984c4f5c007a510b5f71f664ab
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 16:08:30 +0300

    Create program 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < '0' + 10; i++)
+	{
+		putchar(i);
+		if (i == '9')
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}

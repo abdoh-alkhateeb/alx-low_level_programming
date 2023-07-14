454e1b5a19bcdfb9e76064d2aa2e086de4fa553b
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-14 16:04:53 +0300

    Create program 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i < '0' + 10; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i < 'a' + 6; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

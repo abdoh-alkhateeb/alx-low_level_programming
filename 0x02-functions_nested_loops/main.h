dbde83175eb2a05e41c84cff2e65a74ed5f5a9f6
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-20 11:50:49 +0300

    Create main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * Description: a header file that includes function
+ * prototypes for the current project.
+ */
+
+int _putchar(char c);
+
+#endif /* _MAIN_H_ */

7a6b495bf086b2e58adeae8caf259901ee46c8f8
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-20 11:58:12 +0300

    Create program 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet,
+ * in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z' + 1; i++)
+		_putchar(i);
+	_putchar('\n');
+}

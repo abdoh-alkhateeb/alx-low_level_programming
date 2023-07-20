654a63a56e151d0cc2168e73cf33591c85f8752d
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-20 11:53:57 +0300

    Create program 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point of program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char *str = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+	return (0);
+}

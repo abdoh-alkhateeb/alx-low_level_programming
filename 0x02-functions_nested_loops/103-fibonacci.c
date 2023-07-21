5f79a8eaf3b2fcbeaa2e93997f23253cb2f7cf1a
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-21 15:46:55 +0300

    Create program 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int a, b, c;
+	long int sum;
+
+	a = 1, b = 2, c = 3;
+	sum = 2;
+
+	while (c < 4000000)
+	{
+		if (c % 2 == 0)
+			sum += c;
+		a = b;
+		b = c;
+		c = a + b;
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}

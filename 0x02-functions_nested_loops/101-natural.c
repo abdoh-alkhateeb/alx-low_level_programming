910ee52921ef14fdfed1614a7e1466692e9ffebd
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-21 15:01:37 +0300

    Create program 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int result;
+
+	for (i = 0, result = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			result += i;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}

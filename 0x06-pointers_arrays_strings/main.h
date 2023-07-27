95fcf5fe652c57683b8d1d1e2f834eb692bc4211
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-27 21:04:16 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  */
 int _putchar(char);
 char *_strcat(char *, char *);
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
 
 #endif /* _MAIN_H_ */

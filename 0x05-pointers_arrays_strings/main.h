81b5bbac3d52694f578e5ee5d0a89744645d0983
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-07-29 11:55:30 +0300

    Create main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * Description: a header file that includes function
+ * prototypes for the current project.
+ */
+int _putchar(char);
+void reset_to_98(int *);
+
+#endif /* _MAIN_H_ */

dc843f58f23b97b116a5b43e331ff9356a86d9ce
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-03 18:22:09 +0300

    Create main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * Description: a header file that includes function
+ * prototypes for the current project.
+ */
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
+
+#endif /* _MAIN_H_ */

f8751b64249cf71f6d98c1de2e02c28eea8514c2
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-05 10:47:51 +0300

    Create program 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: 0 (always) success.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

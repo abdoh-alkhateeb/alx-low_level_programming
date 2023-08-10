9c20b2b4b1093d66993b92388c248b6d935ba57f
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-10 19:14:09 +0300

    Initialize project 0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * Description: a header file that includes function
+ * prototypes for the current project.
+ */
+int _putchar(char);
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+
+#endif /* _MAIN_H_ */

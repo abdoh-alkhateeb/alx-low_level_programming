dc89207c8373bdaec7cd984583d578bd653802b9
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-16 17:38:04 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,9 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stddef.h>
 
-/**
- * Description: a header file that includes function
- * prototypes for the current project.
- */
-int _putchar(char);
-void print_name(char *, void (*)(char *));
-void array_iterator(int *, size_t, void (*)(int));
-int int_index(int *, int, int (*)(int));
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* _FUNCTION_POINTERS_H_ */

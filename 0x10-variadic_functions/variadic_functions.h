521045fb079665c16ad2ef1c699e88488ca4ca4b
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-17 11:21:03 +0300

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char *const, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */

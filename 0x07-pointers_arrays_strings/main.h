7a4b5b99577c1d951f0a8f4ff920ffbdc4051430
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-17 12:12:34 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-/**
- * Description: a header file that includes function
- * prototypes for the current project.
- */
 int _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
@@ -14,5 +10,6 @@ char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
 void print_chessboard(char (*)[8]);
 void print_diagsums(int *, int);
+void set_string(char **, char *);
 
 #endif /* _MAIN_H_ */

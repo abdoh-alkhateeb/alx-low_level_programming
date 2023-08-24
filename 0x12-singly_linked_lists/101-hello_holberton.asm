5245b7ad582091a4c2d713326857596612cc352c
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-24 20:32:08 +0300

    Create 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    hello: db "Hello, Holberton", 0
+    format: db "%s", 0xa, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rdi, format
+    mov rsi, hello
+    call printf
+    pop rbp
+
+    mov rax, 0
+    ret

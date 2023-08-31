1dc736bcdd5e7c45e8d2720c7987fd686fc06315
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-31 12:31:27 +0300

    Create main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(int);
+unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
+unsigned int flip_bits(unsigned long int, unsigned long int);
+
+#endif /* _MAIN_H_ */

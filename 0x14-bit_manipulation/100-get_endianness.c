044e0e9e710d9c262e81e3d31d475b5cf890f1e4
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-31 20:17:50 +0300

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include <limits.h>
+
+/**
+ * get_endianness - a function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	return (((unsigned char)(UCHAR_MAX + 1) & UCHAR_MAX) == UCHAR_MAX ? 0 : 1);
+}

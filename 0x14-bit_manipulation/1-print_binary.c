54f788df86c66a7858213d8c1e8f50654a41253c
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-08-31 13:11:12 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,15 +8,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 31;
-	unsigned int mask = 1;
+	int i = sizeof(unsigned long int) * 8 - 1;
 
-	while ((mask << i & n) == 0 && i >= 0)
+	while ((1UL << i & n) == 0 && i >= 0)
 		i--;
 
 	if (i < 0)
 		_putchar('0');
 	else
 		for (; i >= 0; i--)
-			_putchar('0' + ((mask << i & n) >> i));
+			_putchar('0' + ((1UL << i & n) >> i));
 }

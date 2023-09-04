0ac4b7a33e6302394095d42b29e49cac558ee461
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-09-04 21:32:10 +0300

    Create main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/types.h>
+
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+
+#endif /* _MAIN_H_ */

d58a592340190cff9863239569dc20258b9b43a8
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-09-04 22:02:22 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 #include <sys/types.h>
 
+int _putchar(int);
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);

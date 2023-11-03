9c9eae9c3541bcb9c9ad6234ef67ac4ee9a3dc6d
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-03 20:45:15 +0200

    Create 5-hash_table_print.c, and update hash_tables.h

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: hash table to print.
+ *
+ * Return: nothing.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	if (ht != NULL)
+	{
+		unsigned long int i;
+		hash_node_t *p;
+		bool first_printf = true;
+
+		printf("{");
+
+		for (i = 0; i < ht->size; i++)
+		{
+			for (p = (ht->array)[i]; p != NULL; p = p->next)
+			{
+				if (first_printf == false)
+					printf(", ");
+
+				printf("'%s': '%s'", p->key, p->value);
+
+				if (first_printf == true)
+					first_printf = false;
+			}
+		}
+
+		printf("}\n");
+	}
+}

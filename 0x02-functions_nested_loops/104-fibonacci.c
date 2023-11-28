9c5b3042c4e65fad29f613604f5d99c1ee7f3628
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-28 20:34:45 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int i;
 	unsigned long int sequence[98];
+	unsigned int carry[8];
 
 	sequence[0] = 1;
 	sequence[1] = 2;
@@ -22,9 +23,21 @@ int main(void)
 		printf("%lu, ", sequence[i]);
 	}
 
+	carry[0] = sequence[i - 2] / 1000000000000000000;
+	carry[1] = sequence[i - 1] / 1000000000000000000;
+
 	for (i = 92; i < 98; i++)
 	{
+		sequence[i - 2] %= 1000000000000000000;
+		sequence[i - 1] %= 1000000000000000000;
 		sequence[i] = sequence[i - 1] + sequence[i - 2];
+
+		carry[i - 92 + 2] = carry[i - 92] + carry[i - 92 + 1];
+
+		carry[i - 92 + 2] += sequence[i] / 1000000000000000000;
+		sequence[i] %= 1000000000000000000;
+
+		printf("%u", carry[i - 92 + 2]);
 		printf(i == 97 ? "%lu\n" : "%lu, ", sequence[i]);
 	}
 

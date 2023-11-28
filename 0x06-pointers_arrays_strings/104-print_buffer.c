6fdaab609bd86a8b20e05a0142d11a4073401256
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-28 18:14:10 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,32 +8,39 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
-
-	for (i = 0; i < size; i += 10)
+	if (size <= 0)
 	{
-		printf("%08x: ", i);
-
-		for (j = 0; j < 10 && i + j < size; j++)
-		{
-			printf("%02x", b[i + j]);
-
-			if ((j + 1) % 2 == 0)
-				printf(" ");
-		}
-
-		for (; j < 11 && i + j >= size; j++)
-			printf("  ");
-
-		for (j = 0; j < 10 && i + j < size; j++)
-		{
-			if (b[i + j] < 32)
-				printf(".");
-
-			else
-				printf("%c", b[i + j]);
-		}
-
 		printf("\n");
+		return;
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%08x: ", i);
+
+			for (j = 0; j < 10; j++)
+			{
+				if (i + j < size)
+					printf("%02x", b[i + j]);
+				else
+					printf("  ");
+
+				if ((j + 1) % 2 == 0)
+					printf(" ");
+			}
+
+			for (j = 0; j < 10 && i + j < size; j++)
+			{
+				if (b[i + j] < 32)
+					printf(".");
+				else
+					printf("%c", b[i + j]);
+			}
+
+			printf("\n");
+		}
 	}
 }

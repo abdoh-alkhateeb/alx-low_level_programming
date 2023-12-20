d0c73c2a944fc8b39ed545e98eb45a96e8c6f027
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-12-21 00:50:00 +0200

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,20 +6,24 @@ Module that defines island_perimeter function.
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island described in the grid.
+    Returns the perimiter of an island.
     """
-    perimeter = 0
-    n = len(grid)
 
-    for i in range(n):
+    n = len(grid[0])
+    m = len(grid)
+
+    a = 0
+    b = 0
+
+    for i in range(m):
         for j in range(n):
             if grid[i][j] == 1:
-                perimeter += 4
+                b += 1
 
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                    a += 1
 
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
+                    a += 1
 
-    return perimeter
+    return b * 4 - a * 2

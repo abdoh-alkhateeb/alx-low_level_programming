f0d00f01245f386db4bdf2830e11f33dc229f417
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-05-16 00:08:51 +0300

    Update 0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,1 +1,1 @@
-0x17. C - Doubly linked lists
+This directory houses code that demonstrates doubly linked lists and related concepts.
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,13 +3,13 @@
 #include <stddef.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
+ * struct dlistint_s - doubly linked list.
+ * @n: integer.
+ * @prev: points to the previous node.
+ * @next: points to the next node.
  *
  * Description: doubly linked list node structure
- * for Holberton project
+ * for Holberton project.
  */
 typedef struct dlistint_s
 {

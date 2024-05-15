5831f9d2895415e555c6156597c5d2c3b2f2cf6f
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-05-15 23:35:16 +0300

    Update 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
+ * _print - moves a string one place to the left and prints the string.
+ * @str: string to move.
+ * @l: size of string.
  *
- * Return: void
+ * Return: void.
  */
 void _print(char *str, int l)
 {
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,1 @@
-Not Empty!
+This directory houses code that demonstrates the use of malloc, free, and related concepts.
